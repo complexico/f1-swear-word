45e29e6380cfa995b64281015c4262ec367a4a10
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2025-05-24 10:36:03 +0800

    Update repo's data and README.

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
+Dataset for “Types of Swear Word Used in 2024 Formula 1 ‘Hungarian GP’
+Race”
+================
+I Putu Dipra Pradnya Nugraha, Gede Primahadi Wijaya Rajeg, Novita
+Mulyana
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# Overview
+## Overview
 
 <!-- badges: start -->
 
+[![](https://img.shields.io/badge/doi-10.17605/OSF.IO/BJF8A-blue.svg)](https://doi.org/10.17605/OSF.IO/BJF8A)
 <!-- badges: end -->
 
-Repository for an undergraduate thesis and article led by I Putu Dipra
-Pradnya Nugraha (Bachelor of English Literature \[BoEL\], Faculty of
-Humanities, Udayana University). The repository can be cited as follows:
+Repository for an undergraduate thesis and a co-authored article led by
+I Putu Dipra Pradnya Nugraha (Bachelor of English Literature \[BoEL\],
+Faculty of Humanities, Udayana University). The repository can be cited
+as follows:
 
-> Nugraha, I. P. D. P., Rajeg, G. P. W., & Mulyana, N. (2025). Dataset
+> Nugraha, I P. D. P., Rajeg, G. P. W., & Mulyana, N. (2025). Dataset
 > for “Types of Swear Word Used in 2024 Formula 1 ‘Hungarian GP’ Race”
 > (Version 1.0.0) \[Data set\].
 > <https://github.com/complexico/f1-swear-word>
+> <https://doi.org/10.17605/OSF.IO/BJF8A>
--- data-retrieval-and-tabulation.R
+++ data-retrieval-and-tabulation.R
@@ -1,8 +1,43 @@
 library(tidyverse)
-source("gsheetlinks.R")
-f1_data <- googlesheets4::read_sheet(gsheetslink,
-                                     sheet = "Sheet1")
-write_tsv(f1_data,
-          file = "f1-data.tsv")
 
-rev(sort(table(f1_data$Swear_Words_Category)))
+# source("gsheetlinks.R")
+# 
+# f1_data <- googlesheets4::read_sheet(gsheetslink,
+#                                      sheet = "Sheet1")
+
+# write_tsv(f1_data,
+#           file = "data-raw/f1-data.tsv")
+
+f1_data <- read_tsv("data-raw/f1-data.tsv")
+
+sw_cat_df <- f1_data |> 
+  count(Swear_Words_Category) |> 
+  mutate(prop = n/sum(n)) |> 
+  mutate(prop = round(prop, digits = 2)) |> 
+  arrange(desc(n))
+
+sw_cat_df
+
+write_tsv(sw_cat_df, "data-out/sw_cat_df.tsv")
+
+sw_term_df <- f1_data |> 
+  count(Swear_Words_Term, name = "n_term") |> 
+  mutate(lemma = str_replace(Swear_Words_Term,
+                             "(ed|ing)$",
+                             ""),
+         lemma = str_to_upper(lemma)) |> 
+  group_by(lemma) |> 
+  mutate(n_lemma = sum(n_term)) |> 
+  ungroup()
+
+write_tsv(sw_term_df, "data-out/sw_term_df.tsv")
+
+sw_term_lemma <- sw_term_df |> 
+  select(matches("lemma")) |> 
+  distinct() |> 
+  ungroup() |> 
+  arrange(desc(n_lemma)) |> 
+  mutate(prop_lemma = n_lemma/sum(n_lemma)) |> 
+  mutate(prop_lemma = round(prop_lemma, digits = 2))
+
+write_tsv(sw_term_lemma, "data-out/sw_term_lemma.tsv")
